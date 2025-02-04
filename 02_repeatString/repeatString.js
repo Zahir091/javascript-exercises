41577c5da842d75231e2842652cac4fa1ee74279
Zahir <176171770+Zahir091@users.noreply.github.com.>
2025-02-04 18:55:49 -0500

    Finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(theString, numOfTimes) {
+    if (numOfTimes < 0) return "ERROR";
+    let newString = "";
+    for(let i = 0; i<numOfTimes; i++) {
+        newString = newString + theString;
+    };
+    return newString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;

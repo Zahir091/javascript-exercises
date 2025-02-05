fca37c447696548803fa1e6abebaa4eac53713ab
Zahir <176171770+Zahir091@users.noreply.github.com.>
2025-02-04 20:05:50 -0500

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...int) {
+    newArray = [];
+    //forEach is a For Of loop for arrays goes through each element and does the specicified thing in the paramter
+    //three possible paramters inside forEach method, (currentElement, index, array)
+
+    //.includes() useful here because we have unlimited arguments and need to check if any of them match array elements
+    arr.forEach((element) => {
+        if(!(int.includes(element))) {
+            newArray.push(element);
+        }; 
+    });
+    if (newArray.includes('')) newArray = arr;
+    return newArray;
 };
 
 // Do not edit below this line

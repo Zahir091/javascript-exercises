f08ce514b4eb8ce7f19834311fff818366169c00
Zahir <176171770+Zahir091@users.noreply.github.com.>
2025-02-04 19:28:11 -0500

    Completed Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = '';
+    let length = string.length;
+    for(let i=0; i<length ; i++) {
+        newString = newString + string.charAt(string.length-1); 
+        string = string.slice(0, -1); //Slice will seperate everything into the string but takes out the last char
+    };
+    return newString;
 };
+
+// return string.split("").reverse().join("");
+// .split("") makes the string an array
+// .reverse() makes the array reverse
+// .join() makes the array a string again
 
 // Do not edit below this line
 module.exports = reverseString;

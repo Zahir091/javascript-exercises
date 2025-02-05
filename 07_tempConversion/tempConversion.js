715b4e26603692265c0c905d2ef0043f4a0b04e7
Zahir <176171770+Zahir091@users.noreply.github.com.>
2025-02-04 23:51:59 -0500

    comment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -4,6 +4,7 @@ const convertToCelsius = function(temp) {
 
 const convertToFahrenheit = function(temp) {
   return Math.round((temp*(9/5) + 32) * 10)/10;
+  //First get the answer, then multiply by then to move decimal one right, then round to cut off other places, then divide 10 to get 1 place
 };
 
 // Do not edit below this line

106385ab6d217f10b318ee05c31dd8d86fdc7771
Zahir <176171770+Zahir091@users.noreply.github.com.>
2025-02-04 23:08:01 -0500

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(integer1, integer2) {
+    let sum = 0;
+    if(!(Number.isInteger(integer1)) || !(Number.isInteger(integer2))) 
+        return "ERROR";
+    if(integer1 < 0 || integer2 < 0)
+        return "ERROR";
+    if(integer1>integer2) {
+        const t = integer1;
+        integer1 = integer2;
+        integer2 = t;
+    }
+    for(let i=integer1; i <= integer2; i++)
+        sum += i;
+    return sum;
+
 
 };
 

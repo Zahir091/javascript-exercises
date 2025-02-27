526a3d92db5de07fa6f9e5508bcd4c27bcb03741
Zahir <176171770+Zahir091@users.noreply.github.com.>
2025-02-27 00:37:32 -0500

    Finished Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(...nums) { //creates an array of parameters
+  return nums.reduce((total, curr) => {
+    return curr + total;
+  },0) 
 };
 
-const subtract = function() {
-	
+const subtract = function(...nums) {
+	return nums.splice(1).reduce((total, curr) => {
+    return total - curr;
+  }, nums[0]);
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((total, curr) => {
+    return curr + total;
+  },0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, curr) => {
+    return total * curr;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(num, expo) {
+  if(expo === 0)
+    return 1;
+	return num * power(num, expo-1);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 1)
+    return 1;
+  if(num === 0)
+    return 1;
+	return num * factorial(num-1);
 };
 
 // Do not edit below this line
